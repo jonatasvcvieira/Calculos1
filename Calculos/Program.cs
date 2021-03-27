03b38d8c219fac61e019c628d7f9832ffce2eb4d
jvasconcelosdev <jonatasvieir@gmail.com>
2021-03-26 21:12:54 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Calculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double soma;
+            string operacao;
+
+            Console.Write("Digite o primeiro numero: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a operação: ");
+            operacao = Console.ReadLine();
+
+            Console.Write("Digite o segundo numero: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            soma = 0;
+
+            if (operacao == "+")
+            {
+                soma = numero1 + numero2;
+            }
+
+            else if (operacao == "-")
+            {
+                soma = numero1 - numero2;
+            }
+
+            else if (operacao == "*")
+            {
+                soma = numero1 * numero2;
+            }
+
+            else if (operacao == "/")
+            {
+                soma = numero1 / numero2;
+            }
+                                  
+            Console.WriteLine("O resultado é " + soma);
+            Console.ReadKey();
+
+        }
+    }
+}
